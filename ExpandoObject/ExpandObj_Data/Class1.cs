261843ec46438d5eed2a78dad7a078bf8f271b58
StephenNnamani <sir.stephennnamani@gmail.com>
2023-01-19 13:58:25 +0100

    Update

---
+++ ExpandoObject/ExpandObj_Data/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace ExpandObj_Data
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ ExpandoObject/ExpandoObj_Model/ProductModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpandoObj_Model
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public int OrderCount { get; set; }
+    }
+}
